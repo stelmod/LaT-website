8a9a5983cf094fc03f753d3e2899ad848f846c9d
Ste <cowley05@hotmail.co.uk>
2017-05-18 21:04:58 +0200

    Ste - Change favicon to LaT logo

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -5,8 +5,8 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/lat-favicon.png" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/lat-touch.png" rel="apple-touch-icon-precomposed">
         <link href="https://fonts.googleapis.com/css?family=Roboto Condensed:100,300,400,700" rel="stylesheet" type="text/css"/>
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
